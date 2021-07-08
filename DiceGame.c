519d63d5fa6866706286b99509c95d6137d74883
市川雄誠 <s1280160@u-aizu.ac.jp>
2021-07-08 23:49:58 +0900

    first commit

---
+++ DiceGame.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void) {
+    srand((unsigned int)time(NULL));
+    char str[256];
+    printf("What is your name?\n");
+    scanf("%s", str);
+    printf("Hello, %s!\n", str);
+    printf("Rolling the dice…\n");
+    int die1 = rand() % 6 + 1;
+    int die2 = rand() % 6 + 1;
+    printf("Die 1:%d\n", die1);
+    printf("Die 2:%d\n", die2);
+    int total = die1 + die2;
+    printf("Total value: %d\n" , total);
+if(total>=7){
+   printf("John won!\n");
+}
+else if(total<7){
+    printf("John lost!\n");
+}
+    return 0;
+}
